b2cf095b772247b1574deb02f683b639c3a7e178
nguyen van phuong <nguyenvanphuong8522@gmail.com>
2026-04-04 16:47:59 +0700

    fix: lay camera.main 1 lan

--- Assets/ECS_SpriteSheetAnim/SpriteSheetRenderer.cs
+++ Assets/ECS_SpriteSheetAnim/SpriteSheetRenderer.cs
@@ -11,32 +11,25 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
     ComputeBuffer uvBuffer;
     ComputeBuffer argsBuffer;
 
-    Matrix4x4[] matrixArray;
-    Vector4[] uvArray;
+
 
     Material material;
     Mesh mesh;
 
+    Camera cam;
     const int MAX = 1_000_000;
-
+    public float height;
+    public float width;
     protected override void OnCreate()
     {
         RequireForUpdate<SpriteSheetAnimationData>();
 
-        matrixArray = new Matrix4x4[MAX];
-        uvArray = new Vector4[MAX];
 
-        matrixBuffer =
-            new ComputeBuffer(MAX, 64);
+        matrixBuffer = new ComputeBuffer(MAX, 64);
 
-        uvBuffer =
-            new ComputeBuffer(MAX, 16);
+        uvBuffer = new ComputeBuffer(MAX, 16);
 
-        argsBuffer =
-            new ComputeBuffer(
-                1,
-                5 * sizeof(uint),
-                ComputeBufferType.IndirectArguments);
+        argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
     }
 
     protected override void OnUpdate()
@@ -45,23 +38,44 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
         if (handler == null) return;
 
+        if (cam == null)
+        {
+            cam = Camera.main;
+
+            height = cam.orthographicSize;
+
+            width = height * cam.aspect;
+        }
+
         mesh = handler.quadMesh;
         material = handler.walkingSpriteSheetMaterial;
 
-        int index = 0;
 
-        Entities
-        .ForEach((
-            in LocalTransform transform,
-            in SpriteSheetAnimationData sprite) =>
+
+        Vector3 camPos = cam.transform.position;
+
+        float minX = camPos.x - width;
+        float maxX = camPos.x + width;
+
+        float minY = camPos.y - height;
+        float maxY = camPos.y + height;
+
+        int index = 0;
+        Matrix4x4[] matrixArray;
+        Vector4[] uvArray;
+
+        matrixArray = new Matrix4x4[MAX];
+        uvArray = new Vector4[MAX];
+        Entities.ForEach((in LocalTransform transform, in SpriteSheetAnimationData sprite) =>
         {
             float3 pos = transform.Position;
 
-            matrixArray[index] =
-                Matrix4x4.TRS(
-                    pos,
-                    Quaternion.identity,
-                    Vector3.one);
+            if (pos.x < minX) return;
+            if (pos.x > maxX) return;
+            if (pos.y < minY) return;
+            if (pos.y > maxY) return;
+
+            matrixArray[index] = Matrix4x4.TRS(pos, Quaternion.identity, Vector3.one);
 
             uvArray[index] = sprite.uv;
 
@@ -71,19 +85,13 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
         if (index == 0) return;
 
-        matrixBuffer.SetData(
-            matrixArray, 0, 0, index);
+        matrixBuffer.SetData(matrixArray, 0, 0, index);
 
-        uvBuffer.SetData(
-            uvArray, 0, 0, index);
+        uvBuffer.SetData(uvArray, 0, 0, index);
 
-        material.SetBuffer(
-            "_Matrices",
-            matrixBuffer);
+        material.SetBuffer("_Matrices", matrixBuffer);
 
-        material.SetBuffer(
-            "_UVData",
-            uvBuffer);
+        material.SetBuffer("_UVData", uvBuffer);
 
         uint[] args = new uint[5]
         {
@@ -96,14 +104,7 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
         argsBuffer.SetData(args);
 
-        Graphics.DrawMeshInstancedIndirect(
-            mesh,
-            0,
-            material,
-            new Bounds(
-                Vector3.zero,
-                Vector3.one * 10000),
-            argsBuffer);
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, new Bounds(Vector3.zero, Vector3.one * 10000), argsBuffer);
     }
 
     protected override void OnDestroy()
