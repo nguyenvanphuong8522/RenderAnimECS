3d47df4f8cdf79b0d11c7a733eac7ae2206f8bd3
nguyen van phuong <nguyenvanphuong8522@gmail.com>
2026-04-04 15:12:44 +0700

    feat: cam move

--- Assets/Scripts/MobileCameraZoom.cs
+++ Assets/Scripts/MobileCameraZoom.cs
@@ -8,8 +8,22 @@ public class MobileCameraZoom : MonoBehaviour
     public float minZoom = 3f;
     public float maxZoom = 2000f;
 
+    public float dragSpeed = 1f;
+
     void Update()
     {
+        // PAN (1 ngón tay)
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                PanCamera(touch);
+            }
+        }
+
+        // ZOOM (2 ngón tay)
         if (Input.touchCount == 2)
         {
             Touch touch0 = Input.GetTouch(0);
@@ -25,6 +39,21 @@ public class MobileCameraZoom : MonoBehaviour
 
             Zoom(difference * zoomSpeed);
         }
+    }
+
+    void PanCamera(Touch touch)
+    {
+        Vector3 prevPos = cam.ScreenToWorldPoint(
+            touch.position - touch.deltaPosition
+        );
+
+        Vector3 currentPos = cam.ScreenToWorldPoint(
+            touch.position
+        );
+
+        Vector3 direction = prevPos - currentPos;
+
+        cam.transform.position += direction * dragSpeed;
     }
 
     void Zoom(float increment)
