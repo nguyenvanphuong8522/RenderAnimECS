ae7bd65b822d1e56da4ca2f4346d5f20d06a2fb9
nguyen van phuong <nguyenvanphuong8522@gmail.com>
2026-04-06 16:36:52 +0700

    fix: cấp phát trong frame

--- Assets/ECS_SpriteSheetAnim/SpriteSheetRenderer.cs
+++ Assets/ECS_SpriteSheetAnim/SpriteSheetRenderer.cs
@@ -11,8 +11,7 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
     private ComputeBuffer uvBuffer;
     private ComputeBuffer argsBuffer;
 
-    NativeArray<Matrix4x4> matrixArray;
-    NativeArray<float4> uvArray;
+
     private NativeArray<uint> args;
 
     private Material material;
@@ -27,9 +26,6 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
         matrixBuffer = new ComputeBuffer(MAX, 64);
         uvBuffer = new ComputeBuffer(MAX, 16);
         argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
-
-        matrixArray = new NativeArray<Matrix4x4>(MAX, Allocator.Persistent);
-        uvArray = new NativeArray<float4>(MAX, Allocator.Persistent);
         args = new NativeArray<uint>(5, Allocator.Persistent);
     }
     protected override void OnDestroy()
@@ -37,12 +33,6 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
         matrixBuffer.Release();
         uvBuffer.Release();
         argsBuffer.Release();
-
-        if (matrixArray.IsCreated)
-            matrixArray.Dispose();
-
-        if (uvArray.IsCreated)
-            uvArray.Dispose();
 
         if (args.IsCreated)
             args.Dispose();
@@ -56,32 +46,33 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
         mesh = gameHandler.quadMesh;
         material = gameHandler.walkingSpriteSheetMaterial;
-        int index = 0;
-        var matrixArrCopy = matrixArray;
-        var uvArrCopy = uvArray;
+
+        var matrices = new NativeList<float4x4>(Allocator.Temp);
+        var uvs = new NativeList<float4>(Allocator.Temp);
         Entities.WithAll<VisibleTag>()
         .ForEach((in LocalTransform transform, in SpriteSheetAnimationData sprite) =>
         {
             float3 pos = transform.Position;
+            matrices.Add(float4x4.TRS(transform.Position, Quaternion.identity, Vector3.one));
+            uvs.Add(sprite.uv);
 
-            matrixArrCopy[index] = Matrix4x4.TRS(pos, Quaternion.identity, Vector3.one);
-
-            uvArrCopy[index] = sprite.uv;
-
-            index++;
         }).Run();
+        int count = matrices.Length;
 
-        if (index == 0) return;
+        if (count == 0) return;
 
-        matrixBuffer.SetData(matrixArrCopy, 0, 0, index);
-
-        uvBuffer.SetData(uvArrCopy, 0, 0, index);
+        matrixBuffer.SetData(matrices.AsArray());
+        uvBuffer.SetData(uvs.AsArray());
 
         material.SetBuffer("_Matrices", matrixBuffer);
         material.SetBuffer("_UVData", uvBuffer);
 
+        matrices.Dispose();
+        uvs.Dispose();
+
+
         args[0] = mesh.GetIndexCount(0);
-        args[1] = (uint)index;
+        args[1] = (uint)count;
         args[2] = mesh.GetIndexStart(0);
         args[3] = mesh.GetBaseVertex(0);
         args[4] = 0;
@@ -90,5 +81,7 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
         Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 10000);
 
         Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
+
+
     }
 }
