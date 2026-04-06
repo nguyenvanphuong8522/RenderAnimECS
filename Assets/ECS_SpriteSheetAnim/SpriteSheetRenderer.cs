67fee8ac7b1df7236f3309f9512084e5a76f718f
nguyen van phuong <nguyenvanphuong8522@gmail.com>
2026-04-06 10:32:13 +0700

    fix; build test

--- Assets/ECS_SpriteSheetAnim/SpriteSheetRenderer.cs
+++ Assets/ECS_SpriteSheetAnim/SpriteSheetRenderer.cs
@@ -7,21 +7,22 @@ using UnityEngine;
 [UpdateInGroup(typeof(PresentationSystemGroup))]
 public partial class SpriteSheetIndirectRenderSystem : SystemBase
 {
-    ComputeBuffer matrixBuffer;
-    ComputeBuffer uvBuffer;
-    ComputeBuffer argsBuffer;
+    private ComputeBuffer matrixBuffer;
+    private ComputeBuffer uvBuffer;
+    private ComputeBuffer argsBuffer;
+
 
     static NativeArray<Matrix4x4> matrixArray;
     static NativeArray<Vector4> uvArray;
-    NativeArray<uint> args;
+    private NativeArray<uint> args;
 
-    Material material;
-    Mesh mesh;
 
-    Camera cam;
+    private Material material;
+    private Mesh mesh;
 
     const int MAX = 1_000_000;
 
+    private Camera cam;
     public float height;
     public float width;
 
@@ -31,22 +32,35 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
         matrixBuffer = new ComputeBuffer(MAX, 64);
         uvBuffer = new ComputeBuffer(MAX, 16);
-
         argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
 
-        // Allocate 1 lần
+
+
         matrixArray = new NativeArray<Matrix4x4>(MAX, Allocator.Persistent);
-
         uvArray = new NativeArray<Vector4>(MAX, Allocator.Persistent);
-
         args = new NativeArray<uint>(5, Allocator.Persistent);
+    }
+    protected override void OnDestroy()
+    {
+        matrixBuffer.Release();
+        uvBuffer.Release();
+        argsBuffer.Release();
+
+        if (matrixArray.IsCreated)
+            matrixArray.Dispose();
+
+        if (uvArray.IsCreated)
+            uvArray.Dispose();
+
+        if (args.IsCreated)
+            args.Dispose();
     }
 
     protected override void OnUpdate()
     {
-        var handler = GameHandler.GetInstance();
+        GameHandler gameHandler = GameHandler.GetInstance();
 
-        if (handler == null) return;
+        if (gameHandler == null) return;
 
         if (cam == null)
         {
@@ -54,20 +68,23 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
             height = cam.orthographicSize;
             width = height * cam.aspect;
+            mesh = gameHandler.quadMesh;
+            material = gameHandler.walkingSpriteSheetMaterial;
         }
 
-        mesh = handler.quadMesh;
-        material = handler.walkingSpriteSheetMaterial;
+
+
+
 
         Vector3 camPos = cam.transform.position;
-
         float minX = camPos.x - width;
         float maxX = camPos.x + width;
 
         float minY = camPos.y - height;
         float maxY = camPos.y + height;
-
         int index = 0;
+
+
 
         Entities
         .ForEach((in LocalTransform transform, in SpriteSheetAnimationData sprite) =>
@@ -85,7 +102,7 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
 
             index++;
 
-        }).Run();
+        }).WithoutBurst().Run();
 
         if (index == 0) return;
 
@@ -101,25 +118,10 @@ public partial class SpriteSheetIndirectRenderSystem : SystemBase
         args[2] = mesh.GetIndexStart(0);
         args[3] = mesh.GetBaseVertex(0);
         args[4] = 0;
-
         argsBuffer.SetData(args);
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.one * 10000);
 
-        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, new Bounds(Vector3.zero, Vector3.one * 10000), argsBuffer);
-    }
 
-    protected override void OnDestroy()
-    {
-        matrixBuffer.Release();
-        uvBuffer.Release();
-        argsBuffer.Release();
-
-        if (matrixArray.IsCreated)
-            matrixArray.Dispose();
-
-        if (uvArray.IsCreated)
-            uvArray.Dispose();
-
-        if (args.IsCreated)
-            args.Dispose();
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
     }
 }
